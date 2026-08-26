2d81b03e970262cf8559a9a80930d9004e73ef53
Germán <germanrodriguezvillar@bonai.es>
2026-08-27 00:18:53 +0200

    ft: app state constructor & destructor

--- crates/hangar-core/src/lib.rs
+++ crates/hangar-core/src/lib.rs
@@ -1,15 +1,44 @@
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool};
 use std::path::PathBuf;
+use tracing::info;
 
 pub struct HangarState {
-    database: PathBuf,
-    data_directory: PathBuf,
+    pub database: PathBuf,
+    pub data: PathBuf,
+    database_pool: SqlitePool,
 }
 
 impl HangarState {
-    pub fn new<P: Into<PathBuf>>(database: P, data_directory: P) -> Self {
-        Self {
-            database: database.into(),
-            data_directory: data_directory.into(),
-        }
+    pub async fn new<P1, P2>(database: P1, data: P2) -> Result<Self, sqlx::Error>
+    where
+        P1: Into<PathBuf>,
+        P2: Into<PathBuf>,
+    {
+        let database = database.into();
+        let data = data.into();
+
+        // Foreign keys constraints enforcement is enabled by default by sqlx
+        // Enable file creation so new databases are handled automatically
+        let database_pool = SqlitePool::connect_with(
+            SqliteConnectOptions::new()
+                .filename(&database)
+                .create_if_missing(true),
+        )
+        .await?;
+
+        info!("Database in use: {}", database.to_string_lossy());
+        info!("Data being served from: {}", data.to_string_lossy());
+
+        Ok(Self {
+            database,
+            data,
+            database_pool,
+        })
+    }
+
+    pub async fn shutdown(self) {
+   		info!("Server shutdown");
+
+    	self.database_pool.close().await
     }
 }
--- crates/hangar-server/src/main.rs
+++ crates/hangar-server/src/main.rs
@@ -1,25 +1,30 @@
-use anyhow::Result;
 use clap::Parser;
-use env_logger::Env;
 use hangar_core::HangarState;
 use std::path::PathBuf;
+use tracing_subscriber::EnvFilter;
 
 #[derive(Parser)]
 #[command(version, about)]
 struct CliArgs {
     #[arg(help = "Database to be used")]
     database: PathBuf,
-    #[arg(help = "Data directory to be served")]
-    data_directory: PathBuf,
+    #[arg(help = "Data to be served")]
+    data: PathBuf,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+// TODO, HANDLE CTRL C GRACEFULLY (CALL SHUTDOWN ON THE APPSTATE STRUCT)
 
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let cli_args = CliArgs::parse();
 
-    let hangar_state = HangarState::new(cli_args.database, cli_args.data_directory);
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| "INFO".into()))
+        .with_level(true)
+        .with_target(true)
+        .init();
+
+    let hangar_state = HangarState::new(cli_args.database, cli_args.data).await?;
 
     Ok(())
 }
